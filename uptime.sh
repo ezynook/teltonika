c7b33124940b91226be78185acb251b79eb4a01d
PasitDev <90300159+ezynook@users.noreply.github.com>
2023-04-07 09:48:27 +0700

    Uptime Check Last Build 2023-04-07

---
+++ uptime.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DATENOW=$(date +'%Y-%m-%d')                          
+UPTIME_NOW=$(gsmctl --modemtime 2 | awk '{print $1}')
+
+if [ "$UPTIME_NOW" == "$DATENOW" ]; then                                       
+    echo "$TODAY -> State Update Check by Date Successfully" >> /var/log/da.log
+else                                                                      
+    echo "$TODAY -> State Update Check by Date Failure" >> /var/log/da.log
+    reboot
+fi
