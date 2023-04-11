1e94858af6758335ee6dd35a6285658ae9b62f2b
PasitDev <90300159+ezynook@users.noreply.github.com>
2023-04-11 16:04:16 +0700

    Update chkservice.sh

--- master/chkservice.sh
+++ master/chkservice.sh
@@ -16,24 +16,6 @@ else
     echo "Last check at: $TODAY -> Service Sim Carrier is Normal" >> /var/log/da.log
 fi
 #
-echo "------------------Check VPN IPSec to HQ----------------------"
-ip="$(ifconfig | grep -A 1 "br-lan" | tail -1 | cut -d ":" -f 2 | cut -d " " -f 1)"
-ping 10.0.255.1 -I $ip -c 3 -q >/dev/null
-ret=$?
-if [ $ret -ne 0 ]; then
-    /etc/init.d/ipsec restart
-    iptables -F
-    iptables -X
-    iptables -P INPUT ACCEPT
-    iptables -P FORWARD ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables-save
-    echo "$TODAY -> Reboot router because IPSec disconnected" >> /var/log/da.log
-else
-    echo "$TODAY -> Service IPSec is Normal"
-    echo "Last check at: $TODAY -> Service IPSec is Normal" >> /var/log/da.log
-fi
-#
 echo "------------------Checking Signal Status (Tier 1 -3C)--------------------"
 SIGNAL1=$(gsmctl -q)
 VALUE1="-100"
@@ -45,23 +27,6 @@ if [ "$SIGNAL1" -le "$VALUE1" ]; then
 else
         echo "Signal is Normal at: ${TODAY}" >> /var/log/check_signal.log
         S_SG="Signal 4G is Good = ${SIGNAL1}"
-fi
-#
-echo "------------------Checking Signal Status (Tier 2 -1C)--------------------"
-ping -c3 10.0.255.1 1>/dev/null 2>/dev/null
-SUCCESS=$?
-if [ $SUCCESS -eq 0 ]; then
-  echo "$TODAY -> Service IPSec is Normal"
-  echo "Last check at: $TODAY -> Service IPSec is Normal" >> /var/log/da.log
-else
-  /etc/init.d/ipsec restart
-  iptables -F
-  iptables -X
-  iptables -P INPUT ACCEPT
-  iptables -P FORWARD ACCEPT
-  iptables -P OUTPUT ACCEPT
-  iptables-save
-  echo "$TODAY -> Reboot router because IPSec disconnected" >> /var/log/da.log
 fi
 #
 sync; echo 3 > /proc/sys/vm/drop_caches
