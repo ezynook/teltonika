7f0aa8577ebb7c737c253958a2b2e90287b923bb
PasitDev <90300159+ezynook@users.noreply.github.com>
2023-04-11 12:32:41 +0700

    Update script_retry.sh

--- master/script_retry.sh
+++ master/script_retry.sh
@@ -30,7 +30,7 @@ fi
 
 echo "Check Sim Signal Level Value Status..."
 SIGNAL1=$(gsmctl -q)
-VALUE1="-90"
+VALUE1="-100"
 
 if [ "$SIGNAL1" -ge "$VALUE1" ]; then
         echo "Loss Signal Restart Device at: ${TODAY}" >> /var/log/check_signal.log
@@ -41,7 +41,7 @@ else
         S_SG="Signal 4G is Good = ${SIGNAL1}"
 fi
 
-echo "${BGreen} Check VPN IPSec (Tier 2 -C1)..."
+echo "Check VPN IPSec (Tier 2 -C1)..."
 ping -c1 10.0.255.1 > /dev/null 2>&1
 SUCCESS=$?
 
@@ -58,24 +58,3 @@ else
   iptables-save
   echo "$TODAY -> Reboot router because IPSec disconnected" >> /var/log/da.log
 fi
-
-echo "Check to connecting to public Google DNS..."
-ping -c3 8.8.8.8 > /dev/null 2>&1
-google_dns=$?
-
-if [ $google_dns -ne 0 ]; then
-        /etc/init.d/ipsec restart
-        /etc/init.d/dnsmasq restart
-        /etc/init.d/network restart
-        iptables -F
-        iptables -X
-        iptables -P INPUT ACCEPT
-        iptables -P FORWARD ACCEPT
-        iptables -P OUTPUT ACCEPT
-        iptables-save
-        echo "$TODAY -> Reboot router because connecting to google dns disconnected" >> /var/log/da.log
-else
-        echo "$TODAY -> Service DNS is Normal"
-        echo "Last check at: $TODAY -> Service DNS is Normal" >> /var/log/da.log
-fi
-
