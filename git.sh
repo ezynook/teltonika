cd9b1c26be0fcc8315d9401182aba759746e23b0
PasitDev <90300159+ezynook@users.noreply.github.com>
2023-04-11 14:00:54 +0700

    Update Code at: 11-04-2023:14-00-54

--- git.sh
+++ git.sh
@@ -2,6 +2,16 @@
 
 TODAY=`date +%d-%m-%Y:%H-%M-%S`
 
+BRANCH=""
+
+if [ -z "$1" ]; then
+    BRANCH="main"
+else
+    BRANCH="$1"
+fi
+
 git add .
-git commit -m "$TODAY"
-git push origin main
+git commit -m "Update Code at: ${TODAY}"
+git push origin $BRANCH
+
+exit 0
